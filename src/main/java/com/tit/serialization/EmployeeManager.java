76f8eb74f7ca8bca506e0b098433e7930dd3b7d5
Bhupendra shakya <bhupendrashakya714@gmail.com>
2025-02-19 15:21:08 +0530

    [Bhupendra Shakya] Add . 04 program of serialization for saving and retreive an object.

---
+++ src/main/java/com/tit/serialization/EmployeeManager.java
@@ -0,0 +1,30 @@
+package com.tit.serialization;
+
+import java.io.*;
+import java.util.List;
+
+public class EmployeeManager {
+    // Method to serialize list of employees to a file
+    public void serializeEmployees(List<Employee> employees, String fileName) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(fileName))) {
+            oos.writeObject(employees);
+            System.out.println("Employees have been serialized to " + fileName);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Method to deserialize list of employees from a file
+    @SuppressWarnings("unchecked")
+    public List<Employee> deserializeEmployees(String fileName) {
+        List<Employee> employees = null;
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(fileName))) {
+            employees = (List<Employee>) ois.readObject();
+            System.out.println("Employees have been deserialized from " + fileName);
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return employees;
+    }
+
+}
